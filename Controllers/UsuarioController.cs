4550e45cfe7f9ddf7940c5def637e0af866b1733
reysonafonso <reysonafonso1979@gmail.com>
2020-05-16 22:05:33 -0400

    Menu Usuario

--- Controllers/UsuarioController.cs
+++ Controllers/UsuarioController.cs
@@ -1,9 +1,53 @@
+using Biblioteca.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Biblioteca.Controllers
 {
     public class UsuarioController : Controller
     {
-        
+        public IActionResult Cadastro()
+        {
+            Autenticacao.CheckLogin(this);
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastro(Usuario u)
+        {
+            UsuarioService usuarioService = new UsuarioService();
+
+            if(u.Id == 0)
+            {
+                usuarioService.Inserir(u);
+            }
+            else
+            {
+                usuarioService.Atualizar(u);
+            }
+
+            return RedirectToAction("Listagem");
+        }
+
+        public IActionResult Listagem(string tipoFiltro, string filtro, int pagina = 1)
+        {
+            Autenticacao.CheckLogin(this);
+            FiltrosUsuarios objFiltro = null;
+            if(!string.IsNullOrEmpty(filtro))
+            {
+                objFiltro = new FiltrosUsuarios();
+                objFiltro.Filtro = filtro;
+                objFiltro.TipoFiltro = tipoFiltro;
+            }
+            UsuarioService usuarioService = new UsuarioService();
+            return View(usuarioService.ListarTodos(objFiltro));
+        }
+
+        public IActionResult Edicao(int id)
+        {
+            Autenticacao.CheckLogin(this);
+            UsuarioService us = new UsuarioService();
+            Usuario u = us.ObterPorId(id);
+            return View(u);
+        }
     }
 }
